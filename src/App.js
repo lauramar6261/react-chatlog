b636da3762c6a2f835848e730d2bc8d602c64512
Ada <larmartinez2@gmail.com>
2018-11-27 14:31:52 -0800

    added names to the title

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import './App.css';
 import chatMessages from './data/messages.json';
 import Message from './components/Message'
 
-const data = [
-  {
-    "sender":"Vladimir",
-    "body":"why are you arguing with me",
-    "timeStamp":"2018-05-29T22:49:06+00:00"
-  }
-]
+// const data = [
+//   {
+//     "sender":"Vladimir",
+//     "body":"why are you arguing with me",
+//     "timeStamp":"2018-05-29T22:49:06+00:00"
+//   }
+// ]
 
 class App extends Component {
   render() {
     console.log(chatMessages);
+    let sender1 = chatMessages[0].sender
+    let sender2 = chatMessages[1].sender
     const statusComponents = chatMessages.map((status) => {
             return < Message
             sender={status.sender}
@@ -24,7 +26,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Application title</h1>
+          <h1 className="App-title"> Chat between {sender1} and {sender2}</h1>
         </header>
         <main className="App-main">
         {statusComponents}
