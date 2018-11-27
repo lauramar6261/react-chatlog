e96964249ebfb31703f36811c91105aabf67d422
Ada <larmartinez2@gmail.com>
2018-11-27 13:49:28 -0800

    added timestamp

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Message.css';
+import Timestamp from './Timestamp';
 
 
 const Message = (props) => {
 
   const sender = props.sender
   const body = props.body
-  const timeStamp = props.timeStamp
+  const timeStamp = Timestamp(props.timeStamp)
 
   return (
     <div className="chat-entry local">
