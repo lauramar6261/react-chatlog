349674790467b2a685c0f2ddb38a8649f05fb917
Ada <larmartinez2@gmail.com>
2018-11-27 14:16:12 -0800

    added local or remote as appropriate

--- src/components/Message.js
+++ src/components/Message.js
@@ -9,15 +9,27 @@ const Message = (props) => {
   const body = props.body
   const timeStamp = Timestamp(props.timeStamp)
 
-  return (
-    <div className="chat-entry local">
-    <p className="entry-name">{sender}</p>
-      <article className="entry-bubble">
-      <p className="entry-body">{body}</p>
-      <p className="entry-time">{timeStamp}</p>
-      </article>
-    </div>
-  );
+  if (sender === "Vladimir" ) {
+     return (
+        <div className="chat-entry local">
+        <p className="entry-name">{sender}</p>
+          <article className="entry-bubble">
+          <p className="entry-body">{body}</p>
+          <p className="entry-time">{timeStamp}</p>
+          </article>
+        </div>
+     )
+   } else {
+     return (
+       <div className="chat-entry remote">
+       <p className="entry-name">{sender}</p>
+         <article className="entry-bubble">
+         <p className="entry-body">{body}</p>
+         <p className="entry-time">{timeStamp}</p>
+         </article>
+       </div>
+     );
+   }
 };
 
 
